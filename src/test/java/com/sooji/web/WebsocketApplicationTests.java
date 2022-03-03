274db153992b071c29aa85e6f9aefa0a19e39f42
dobbysj <dobbysj@gmail.com>
2022-03-03 15:49:02 +0900

    Initial commit

---
+++ src/test/java/com/sooji/web/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooji.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
